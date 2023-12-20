c55da0f6a4187b244e2bd1142e3fb862eddad040
DianaTuei <memotuei@gmail.com>
2023-12-20 15:06:31 +0300

    Corrected Challenge 2 and 3

--- js/speedDetector.js
+++ js/speedDetector.js
@@ -2,22 +2,21 @@
 function checkSpeedLimit(speed){
     // creates variables for speedLimit,demerit points given after 5km over a certain speed limit and the point where one gets suspended
     const speedLimit = 70;
-    const demeritForEvery5km = 1;
     const suspensionMark = 12;
 
     // set a consition to determine whether speed one is driving at is greater than speedlimit set.
-    if (speed < speedLimit){
+    if (speed <= speedLimit){
         console.log("OK");
     } else {
         // calculates demerit points
-        const demeritPoints = math.floor((speed - speedLimit) / 5);
+        const demeritPoints = Math.floor((speed - speedLimit) / 5);
         console.log(`Points: ${demeritPoints}`);
         // defines the point where a drivers license gets suspended
         if (demeritPoints > suspensionMark){
-            console.log("License Suspended")
+            console.log("License Suspended");
         }
     }
 }
 
-const speedOfCar = prompt("Car speed");
-checkSpeedLimit(parseInt(speedOfCar));
+const speedOfCar = 150;
+checkSpeedLimit(speedOfCar);
